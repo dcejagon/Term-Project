083b577ce239ead9642bab50942e34c4261e54fb
nzclapp <nzclapp@calpoly.edu>
2022-03-15 13:55:54 -0700

    uplaod new

---
+++ docs/search/files_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['gcodeinterpreter_2epy_0',['GcodeInterpreter.py',['../GcodeInterpreter_8py.html',1,'']]]
+];
---
+++ docs/search/variables_b.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['r_0',['R',['../classGcodeInterpreter_1_1GcodeInterpreter.html#a9d72588d3eadf398ebac47e5304c549f',1,'GcodeInterpreter::GcodeInterpreter']]],
+  ['rcontrol_1',['RControl',['../main_8py.html#a874c858cb654417e02a4f0db017e74e7',1,'main']]],
+  ['rgo_2',['RGO',['../classRMotorControl_1_1RClosedLoop.html#a9ec65ac509fb5fcacb599c918202cbfa',1,'RMotorControl.RClosedLoop.RGO()'],['../main_8py.html#abd2743711754e3648f956f6c3889ab8d',1,'main.RGO()']]],
+  ['rswitch_3',['Rswitch',['../classServoMotorF_1_1LimitSwitch.html#a64871235c1be2c0919c06b5e6d259011',1,'ServoMotorF.LimitSwitch.Rswitch()'],['../main_8py.html#a2710766abdf6f3767409cc7642c71148',1,'main.Rswitch()']]]
+];
