e223b80b57fa031f62e724d5ca49e76428d517ee
nzclapp <nzclapp@calpoly.edu>
2022-03-15 13:51:39 -0700

    delte again

--- docs/search/variables_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['duty1_0',['duty1',['../classMotorDriver_1_1MotorDriver.html#aa08d728dc67065a6625263df36b3bcd4',1,'MotorDriver.MotorDriver.duty1()'],['../main_8py.html#a4f911a1828df7baf0a3e060305223057',1,'main.duty1()']]],
-  ['duty2_1',['duty2',['../classMotorDriver_1_1MotorDriver.html#a6a019e9f693696927fc6c98e7c0be6e9',1,'MotorDriver.MotorDriver.duty2()'],['../classRMotorControl_1_1RClosedLoop.html#a972930b0ccf7f182a555374de3ae968f',1,'RMotorControl.RClosedLoop.duty2()'],['../classServoMotorF_1_1LimitSwitch.html#ae6ef0f4e400069a3921c37cb0799be4d',1,'ServoMotorF.LimitSwitch.duty2()'],['../main_8py.html#ac52b4d68543efdb75c86ea26b8171085',1,'main.duty2()']]]
-];
