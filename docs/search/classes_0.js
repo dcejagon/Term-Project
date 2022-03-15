e5bcde0d7f7fcd17f41c85d30c5ff426d1f36e84
nzclapp <nzclapp@calpoly.edu>
2022-03-15 13:39:00 -0700

    ashdhfljk

--- docs/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['baseshare_0',['BaseShare',['../classtask__share_1_1BaseShare.html',1,'task_share']]]
-];
--- docs/search/files_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['encoderdriver_2epy_0',['EncoderDriver.py',['../EncoderDriver_8py.html',1,'']]]
-];
