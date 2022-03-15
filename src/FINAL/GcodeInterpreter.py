d9dd341b4b1f5a41323520e0f0d6979a8ff904d2
cmkep <97115665+cmkep@users.noreply.github.com>
2022-03-14 23:55:00 -0700

    Update GcodeInterpreter.py

--- src/FINAL/GcodeInterpreter.py
+++ src/FINAL/GcodeInterpreter.py
@@ -1,9 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  4 17:18:34 2022
-
-@author: kepte
-"""
+''' @file                       GcodeInterpreter.py
+    @brief                      File which interprets gcode and converts values into usable data for motor encoders.
+    @details                    Opens a gcode file and extracts position data and converts to polar coordinates. Plots data for verification and writes data to two text files to be used by the onboard program.
+                                
+    @author                     Daniel Gonzalez
+    @author                     Nolan Clapp
+    @author                     Caleb Kephart
+    @date                       March 14, 2022
+'''
 from matplotlib import pyplot as plt
 import math
 import time  
@@ -11,22 +14,33 @@ class GcodeInterpreter:
     
     #def __init__(self,ThetaArray,RArray,setpoint1,setpoint2):
     def __init__(self):
-        '''! initializes class and  arrays for data collection
+        '''! initializes class and arrays for data collection for generated text documents
         '''
         # self.ThetaArray = ThetaArray
         # self.RArray = RArray
         # self.setpoint1 = setpoint1
         # self.setpoint2 = setpoint2
+        
+        ## @brief  Array set up to collect data points for the arm angle position.
+        #
         self.Theta = [] 
+        ## @brief  Array set up to collect data points for the position along the arm.
+        #
         self.R = []
+        ## @brief Array set up to collect arm angle data converted to polar coordinates.
+        #
         self.Thetaradi = []
       
         
     def read (self,fileName):
         '''! opens the text file and reads all data contained
              goes line by line, searching for lines with relevant x and y coord data. extracts data and stores it in list.
+             
+             @param fileName: file name of input gcode
+             @return mylines: array of relevant lines of gcode.
         '''
-        
+        ## @brief Extracts relevant lines from gcode to acquire x and y coordinates
+        #.
         self.mylines = []
         with open(fileName) as f:
             for line in f:
@@ -40,6 +54,7 @@ class GcodeInterpreter:
   
     def XExtract (self):
         '''! Extracts the X values from list of data and floats elements.
+            @return x: Extracted x coordinates from gcode
         '''
         self.x = []
         self.Theta = []
@@ -55,6 +70,7 @@ class GcodeInterpreter:
         
     def YExtract (self):
         '''! Extracts the Y values from list of data and floats elements.
+            @return y: Extracted y coordinates from gcode
         '''
         self.y = []
         for line in self.mylines:
@@ -67,6 +83,7 @@ class GcodeInterpreter:
     
     def ZConvert (self):
         '''! Extracts Z values and converts into values for servo to read to engage/disengage.
+            @return z: Extracted z plunging positions from gcode.
         '''
         self.z = []
         for line in self.mylines:
@@ -100,14 +117,14 @@ class GcodeInterpreter:
         
     
         
-    def ShareGen (self):
-        '''! Takes converted data and passes it into position shares one line at a time to be used by motor controllers.
-        '''
-        for line in self.RArray:
-            self.setpoint1.put(self.ThetaArray)
-            self.setpoint2.put(self.RArray)
+    # def ShareGen (self):
+    #     '''! Takes converted data and passes it into position shares one line at a time to be used by motor controllers.
+    #     '''
+    #     for line in self.RArray:
+    #         self.setpoint1.put(self.ThetaArray)
+    #         self.setpoint2.put(self.RArray)
             
-        yield self.setpoint1, self.setpoint2
+    #     yield self.setpoint1, self.setpoint2
         
             
         
@@ -124,7 +141,7 @@ class GcodeInterpreter:
           plt.show()
           
     def plotpol (self):
-        '''! Generates plot of interpreted gcode using polar coordinates
+        '''! Generates plot of interpreted gcode using polar coordinates.
         '''
         plt.polar(self.Thetaradi,self.R)
         plt.show
